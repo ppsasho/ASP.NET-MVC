4d214f26a700f6004e52a127dc7662ef8ef6f28b
ppsasho <businessmail5545@gmail.com>
2024-06-05 20:55:43 +0200

    - schoolwork class 02

---
+++ schoolwork/class 02/SecondMVCApp/SecondMVCApp/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecondMVCApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index() {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult PlainText()
+        {
+            return Content("Random text message appeared?");
+        }
+
+        public IActionResult JsonAction() 
+        {
+            List<string> userList  = new List<string>() { "Sasho Popovski", "David Davidsky" };
+
+            return Json(userList);
+        }
+    }
+}
