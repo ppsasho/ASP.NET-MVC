4c5a57843ea5f740af92957c5629491982ae478f
ppsasho <businessmail5545@gmail.com>
2025-04-03 15:20:26 +0200

    done

---
+++ homework/Academy Mangement App/Academy Management App/Services/Implementations/TrainerService.cs
@@ -0,0 +1,27 @@
+﻿using Data_Access.Interfaces;
+using Domain_Models;
+using Services.Interfaces;
+
+namespace Services.Implementations
+{
+    public class TrainerService : ITrainerService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public TrainerService(IUnitOfWork unitOfWork) 
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public List<Student> GetStudents(int trainerId) 
+        {
+            var students = new List<Student>();
+            var trainer = _unitOfWork.UserRepository.GetById(trainerId);
+            if (trainer == null)
+            {
+                //var trainersStudents = _unitOfWork.StudentRepository.GetAll().Where(x => x.Subjects.Any(x => x.SubjectName ==);
+            }
+            return students;
+        }
+
+    }
+}
